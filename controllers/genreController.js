628487dab15d2394955ca822b946ed249dccdcaa
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2023-01-04 09:48:43 -0500

    Update genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -7,7 +7,7 @@ const { body, validationResult } = require("express-validator");
 // Display list of all Genre.
 exports.genre_list = function (req, res, next) {
   Genre.find()
-    .sort({name: 1})
+    .sort({ name: 1 })
     .exec(function (err, list_genres) {
       if (err) {
         return next(err);
@@ -20,7 +20,6 @@ exports.genre_list = function (req, res, next) {
     });
 };
 
-// Display detail page for a specific Genre.
 // Display detail page for a specific Genre.
 exports.genre_detail = (req, res, next) => {
   async.parallel(
@@ -53,12 +52,10 @@ exports.genre_detail = (req, res, next) => {
   );
 };
 
-
 // Display Genre create form on GET.
 exports.genre_create_get = (req, res, next) => {
   res.render("genre_form", { title: "Create Genre" });
 };
-
 
 // Handle Genre create on POST.
 exports.genre_create_post = [
@@ -172,11 +169,78 @@ exports.genre_delete_post = (req, res, next) => {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+exports.genre_update_get = (req, res, next) => {
+  // Get genre for form.
+  Genre.findById(req.params.id).exec(function (err, genre_found) {
+    if (err) {
+      return next(err);
+    }
+    if (genre_found == null) {
+      // No results.
+      const err = new Error("Genre not found");
+      err.status = 404;
+      return next(err);
+    }
+    // Success.
+    res.render("genre_form", {
+      title: "Update Genre",
+      genre: genre_found,
+    });
+  });
 };
 
 // Handle Genre update on POST.
 exports.genre_update_post = (req, res) => {
   res.send("NOT IMPLEMENTED: Genre update POST");
 };
+
+exports.genre_update_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name required").trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Genre object with escaped/trimmed data and old id.
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all genre name for form.
+      Genre.findById(req.params.id).exec(function (err, genre_found) {
+        if (err) {
+          return next(err);
+        }
+        if (genre_found == null) {
+          // No results.
+          const err = new Error("Genre not found");
+          err.status = 404;
+          return next(err);
+        }
+        // Success.
+        res.render("genre_form", {
+          title: "Update Genre",
+          genre: genre_found,
+          errors: errors.array(),
+        });
+      });
+      return;
+    }
+
+    // Data from form is valid. Update the record.
+    Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+      if (err) {
+        return next(err);
+      }
+
+      // Successful: redirect to book detail page.
+      res.redirect(thegenre.url);
+    });
+  },
+];
