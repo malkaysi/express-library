f80c74771ff5db365b65086bb6d53e9d0fc81ad5
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2023-01-04 19:30:30 -0500

    Updating MongoDB URI

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ dotenv.config()
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = process.env.DATABASE_URL;
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
